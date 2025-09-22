724f76a6fd31c93ba370a43c85a45c2d2f9f1656
determaer <determaer2012@gmail.com>
2025-09-22 15:15:17 +0700

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
+  ssr:false,
   devtools: { enabled: true },
   css: ['~/assets/index.css'],
   app:{
-    baseURL: '/cat-catalogue/'
+    baseURL: '/cat-catalogue/',
   },
   nitro: {
     preset: 'github_pages'
