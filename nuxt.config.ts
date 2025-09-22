4e3f7271ff6654343d62d455592412fb75358c7c
determaer <determaer2012@gmail.com>
2025-09-22 14:54:01 +0700

    fix: base url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,8 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  css: ['~/assets/index.css']
+  css: ['~/assets/index.css'],
+  app:{
+    baseURL: '/cat-catalogue/'
+  }
 })
